177f5c2d93eb476da53c9696f5ac0e89880f68b7
Tim M <timothy.romero.manas@gmail.com>
2021-04-21 13:47:59 -0400

    Adding Store

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { articleListReducer } from './reducers/articleReducers'
+
+const reducer = combineReducers({
+  articleList: articleListReducer,
+})
+
+const initialSate = {}
+
+const middleware = [thunk]
+const store = createStore(
+  reducer,
+  initialSate,
+  combineReducers(applyMiddleware(...middleware))
+)
+
+export default store
